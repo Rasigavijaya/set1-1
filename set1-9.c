76e91aa187049a427eaab66225fae737edc29e61
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2018-12-23 15:01:01 +0530

    Create set1-9.c

---
+++ set1-9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a[20],n,i,sum=0,m;
+ clrscr();
+ printf("Enter your range:");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+ for(i=0;i<k;i++)
+ {
+  sum=sum+i;
+ }
+ printf("%d",sum);
+ getch();
+ }
