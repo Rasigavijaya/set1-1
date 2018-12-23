410b28f4d231149fe49e7ea1a726a4fa28be9155
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2018-12-23 15:23:58 +0530

    Create set2-2.c

---
+++ set2-2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,rem,temp,t=0,m;
+ clrscr();
+ scanf("%d",n);
+ m=n;
+ while(n!=0)
+ {
+  rem=n%10;
+  t=t*10+r;
+  n=n/10;
+ }
+ if(t==m)
+ {
+  printf("palindrom");
+ }
+ else
+ {
+  printf("not palindrom");
+ }
+ getch();
+ }
