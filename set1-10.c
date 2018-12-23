268182ab424ae4880b22d3480afb33b4b14d1579
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2018-12-23 15:07:30 +0530

    Create set1-10.c

---
+++ set1-10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,i,rem,sum=0;
+ clrscr();
+ printf("Enter the number:");
+ scanf("%d",&n);
+ while(n!=0)
+ {
+  rem=n%10;
+  sum=sum+rem;
+  n=n/10;
+ }
+ printf("%d",sum);
+ getch();
+ }
