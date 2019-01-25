0397877bab1d4e5a4cb5e5ba387b460e8de0fd4a
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-01-25 20:20:10 +0530

    Create set57.c

---
+++ set57.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+ int a[30],i,max,min,n;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+ max=a[0];
+ for(i=0;i<n;i++)
+ {
+  if(a[i]>max)
+  {
+   max=a[i];
+  }
+ }
+ printf("%d",max);
+ min=a[0];
+ for(i=0;i<n;i++)
+ {
+  if(a[i]<min)
+  {
+   min=a[i];
+  }
+ }
+ printf("%d",min);
+} 
+ 
+  
