ae4c267d7cc45ace6b0d357a6ee60089b3c4de8f
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-01-25 19:59:03 +0530

    Update set53.c

--- set53.c
+++ set53.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 void main()
 {
- char a[50],b[50];
+ char a[50],b[50],c[50];
  int i,j=0;
  gets(a);
+ gets(c);
  for(i=0;a[i]!='\0';i++)
  {
-  if(a[i]!=" ")
-  {
    b[j]=a[i];
    j++
-  }
-  else
-  {
-  i++;
-  }
+ }
+ for(i=0;c[i]!='\0';i++)
+ {
+   b[j]=a[i];
+   j++
+ }
   b[j]='\0';
   printf("%s",b);
   }
